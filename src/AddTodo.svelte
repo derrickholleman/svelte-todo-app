7a0b14c748bf4129d4525eb2a07e87900caf7629
Derrick Holleman <derrickholleman@gmail.com>
2022-01-29 22:11:07 -0600

    button classes

--- src/AddTodo.svelte
+++ src/AddTodo.svelte
@@ -2,10 +2,22 @@
   import { v4 as uuidv4 } from "uuid";
   import { todos } from "./stores/todoStore";
 
+  let btnDisabled = true;
   let text;
+
+  let btnDisabledClass =
+    "block mx-auto py-2 px-3 my-3 bg-green-600 border rounded-md text-white opacity-50 cursor-default";
+  let btnActiveClass =
+    "block mx-auto py-2 px-3 my-3 bg-green-600 hover:bg-green-500 border rounded-md text-white";
 
   const handleInput = (e) => {
     text = e.target.value;
+
+    if (text.length >= 5) {
+      btnDisabled = false;
+    } else {
+      btnDisabled = true;
+    }
   };
 
   function addTodo() {
@@ -27,10 +39,11 @@
     on:input={handleInput}
     id="addTodo"
     class="border-black border rounded-md pl-1"
+    required
   />
   <button
     type="submit"
-    class="block mx-auto py-2 px-3 my-3 bg-green-600 hover:bg-green-500 border rounded-md text-white"
-    >Submit</button
+    class={btnDisabled ? btnDisabledClass : btnActiveClass}
+    disabled={btnDisabled}>Submit</button
   >
 </form>
