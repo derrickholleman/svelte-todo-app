a3ab6e8f0fabe3474bd286b2983b04dea7569e42
Derrick Holleman <derrickholleman@gmail.com>
2022-01-30 15:30:34 -0600

    able to delete todos

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -17,10 +17,16 @@
       return copiedTodos;
     });
   };
+
+  const handleDeleteTodo = (todoId) => {
+    todos.update((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
 </script>
 
 <div
-  class="text-center border-black border-2 my-4 mx-auto p-2 w-3/4 lg:w-1/2 rounded-md"
+  class="text-center border-black border-2 my-4 mx-auto p-2 w-3/4 lg:w-1/2 rounded-md relative"
 >
   <p class={isCompleted ? checkedClass : uncheckedClass}>{text}</p>
   <div class="flex items-center justify-center">
@@ -32,4 +38,9 @@
       id={`complete-todo-${id}`}
     />
   </div>
+  <button
+    class="absolute -top-1 right-1 text-3xl text-red-600 hover:text-red-300"
+    aria-label="delete todo"
+    on:click={() => handleDeleteTodo(id)}>X</button
+  >
 </div>
